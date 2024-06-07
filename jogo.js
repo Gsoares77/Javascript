4e5351d5e093167f58852d76553f8c8817d4f7c9
GERSON SOARES DO NASCIMENTO <a95286399@rn.senac.local>
2024-06-07 10:25:21 -0300

    exito ao fazer fundo de tela degrade

--- jogo.js
+++ jogo.js
@@ -11,6 +11,24 @@ function jogar (celula){
     }
 }
 
+
+function checkWin() {
+   var cells = document.getElementsByTagName("td");
+   var winPatterns = [
+       [0, 1, 2], [3, 4, 5], [6, 7, 8], // Linhas
+       [0, 3, 6], [1, 4, 7], [2, 5, 8], // Colunas
+       [0, 4, 8], [2, 4, 6]             // Diagonais
+   ];
+
+   for (var i = 0; i < winPatterns.length; i++) {
+       var [a, b, c] = winPatterns[i];
+       if (cells[a].innerHTML !== "" && cells[a].innerHTML === cells[b].innerHTML && cells[a].innerHTML === cells[c].innerHTML) {
+           return true;
+       }
+   }
+   return false;
+}
+
 function reiniciar(){
          window.location.reload();
 }
